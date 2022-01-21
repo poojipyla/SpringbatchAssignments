93618a0e6300d924c9a41f87a33160f9b6171c3e
poojipyla <91530997+poojipyla@users.noreply.github.com>
2022-01-21 13:49:53 +0530

    Add files via upload

---
+++ assignment2/assignment2/src/main/java/com/mysqltocsv/config/UserRowMapper.java
@@ -0,0 +1,21 @@
+package com.mysqltocsv.config;
+
+import com.mysqltocsv.model.User;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserRowMapper implements RowMapper<User> {
+
+    @Override
+    public User mapRow(ResultSet rs, int rowNum) throws SQLException{
+        User user=new User();
+        user.setGender(rs.getString("gender"));
+        user.setRace(rs.getString("race"));
+        user.setParental_level_of_education(rs.getString("parental_level_of_education"));
+        user.setTest_preparation_course(rs.getString("test_preparation_course"));
+        user.setMath_score(rs.getInt("math_score"));
+        return user;
+    }
+}
---
+++ assignment2/assignment2/src/main/java/com/mysqltocsv/writer/CsvWriter.java
@@ -0,0 +1,19 @@
+package com.mysqltocsv.writer;
+
+import com.mysqltocsv.model.User;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
+import org.springframework.core.io.ClassPathResource;
+
+public class CsvWriter extends FlatFileItemWriter<User>{
+    public CsvWriter() {
+        this.setResource(new ClassPathResource("data.csv"));
+        DelimitedLineAggregator<User> lineAggregator=new DelimitedLineAggregator<User>();
+        lineAggregator.setDelimiter(",");
+        BeanWrapperFieldExtractor<User> fieldExtractor=new BeanWrapperFieldExtractor<User>();
+        fieldExtractor.setNames(new String[]{"gender","race","parental_level_of_education","test_preparation_course","math_score"});
+        lineAggregator.setFieldExtractor(fieldExtractor);
+        this.setLineAggregator(lineAggregator);
+    }
+}
